b7a2656ae1bcd2ab1e0b1a456261b050d2f0f555
Jmatam5k <jmatam.go@gmail.com>
2016-12-19 13:00:42 -0300

    etapa tres lista, agregado los objetos que cumplen la propiedad

--- js/script-index.js
+++ js/script-index.js
@@ -11,9 +11,15 @@ $(document).ready( function(){
 * marcado el atributo "highlighted" como TRUE
 */
 function renderHighlightedRecipes(recipesArray) {
-	console.log('Recipes: ', recipesArray);
+	//console.log('Recipes: ', recipesArray);
+	for(var i=0 ; i < recipesArray.length ; i++){
+		if (recipesArray[i].highlighted == true) {
+			renderRecipe(recipesArray[i]);
+			console.log('Recipes: ', recipesArray);
+		}
+	}
 }
-
+	
 /*
 * Función que se encarga de pintar UNA recetas que tenga 
 * marcado el atributo "highlighted" como TRUE
